677ff41cc10ccf801e7e433eb7d527796c294c88
Luke VanderHart <luke@vanderhart.net>
2014-05-23 11:50:23 -0400

    Remove examples dir and build, since there was nothing there anyway

--- examples/src/quiescent/examples/hello.cljs
+++
@@ -1,6 +0,0 @@
-(ns quiescent.examples.hello
-  (:require [clojure.browser.repl]))
-
-(defn ^:export main
-  []
-  (js/alert "hello, world!"))
--- project.clj
+++ project.clj
@@ -4,25 +4,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6"]
                  [com.facebook/react "0.9.0"]]
-  :source-paths ["src"]
-
-  ;; development concerns
-  :profiles {:dev {:source-paths ["src" "examples/src"]
-                   :resource-paths ["examples/resources"]
-                   :plugins [[lein-cljsbuild "1.0.1"]]
-                   :dependencies [[org.clojure/clojure "1.5.1"]
-                                  [org.clojure/clojurescript "0.0-2156"]]
-                   :cljsbuild
-                   {:builds
-                    [{:source-paths ["src" "examples/src"]
-                      :compiler
-                      {:output-to "examples/resources/public/main.js"
-                       :output-dir "examples/resources/public/build"
-                       :optimizations :whitespace
-                       :preamble ["react/react.min.js"]
-                       :externs ["react/externs/react.js"]
-                       :pretty-print true
-                       :source-map
-                       "examples/resources/public/main.js.map"
-                       :closure-warnings {:non-standard-jsdoc :off}}}]}}})
+  :source-paths ["src"])
 
