4f4781157587ad337459de88ef2780c5db27b983
Luke VanderHart <luke@vanderhart.net>
2014-05-23 15:07:03 -0400

    release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quiescent "0.1.2-SNAPSHOT"
+(defproject quiescent "0.1.2"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
