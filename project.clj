73a9fda7a69940c60d2ff66c62782a8d9b37acec
Luke VanderHart <luke@vanderhart.net>
2014-05-23 15:09:54 -0400

    add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject quiescent "0.1.2"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
+  :url "http://github.com/levand/quiescent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
