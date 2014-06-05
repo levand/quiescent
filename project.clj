771ea5c5fba9de3c54c50f2c778551d2d77d577e
Luke VanderHart <luke@vanderhart.net>
2014-06-05 10:31:04 -0400

    release 0.1.3

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject quiescent "0.1.2"
+(defproject quiescent "0.1.3"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
   :url "http://github.com/levand/quiescent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src"])
-
