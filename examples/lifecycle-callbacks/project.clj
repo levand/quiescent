18cf0e42d19583b2a295c064ecd7b19da52161d0
Luke VanderHart <luke@vanderhart.net>
2016-02-19 11:33:05 -0500

    Update Clojure, ClojureScript & lein-cljsbuild versions

--- examples/lifecycle-callbacks/project.clj
+++ examples/lifecycle-callbacks/project.clj
@@ -1,7 +1,7 @@
 (defproject quiescent.examples.lifecycle-callbacks "0.1.0"
-  :plugins [[lein-cljsbuild "1.0.4"]]
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-2843"]
+  :plugins [[lein-cljsbuild "1.1.2"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [quiescent/quiescent "0.2.0-RC1"]]
   :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/gen"]
   :cljsbuild {:builds
--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject quiescent "0.2.0-RC2"
+(defproject quiescent "0.3.0-SNAPSHOT"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
   :url "http://github.com/levand/quiescent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-2760"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [cljsjs/react-with-addons "0.13.3-0"]]
   :source-paths ["src"])
